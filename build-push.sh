525bbcf92f71baa042f0e8952cabe40340362882
Chris Wieringa <chris@wieringafamily.com>
2024-10-03 16:39:52 +0000

    updated build

--- build-push.sh
+++ build-push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Build the base container first, and push it
-docker buildx build --no-cache --platform arm64 -t chriswier/mikrotik-ubuntu:latest . --push --output type=docker
+docker buildx build --no-cache --platform arm64 -t chriswier/mikrotik-ubuntu:latest -o type=docker,dest=- . > output/mikrotik-ubuntu.tar
